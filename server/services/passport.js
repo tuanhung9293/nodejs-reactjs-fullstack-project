ac84ca52b8378f80aa54f791caaf35355fdb6008
Tuan Ho <tho@openwt.com>
2018-01-19 23:11:57 +0700

    fix proxy

--- server/services/passport.js
+++ server/services/passport.js
@@ -21,7 +21,8 @@ passport.use(
         {
             clientID: keys.googleClientID,
             clientSecret: keys.googleClientSecret,
-            callbackURL: '/auth/google/callback'
+            callbackURL: '/auth/google/callback',
+            proxy: true
         },
         (accessToken, freshToken, profile, done) => {
             User.findOne({googleId: profile.id})
